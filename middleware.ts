9c6969e514f1645eb9a1362287b0e90fb646ce67
Narawit1707 <cutter7ven@gmail.com>
2024-05-08 04:44:50 +0700

    8

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,10 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
-  
+export default clerkMiddleware({
+  //publicRoutes: ["/api/webhook"],
+});
+
 export const config = {
+  // ระบุเงื่อนไขการเรียกใช้ clerkMiddleware
   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
